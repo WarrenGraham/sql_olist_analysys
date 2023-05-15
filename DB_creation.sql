1ca1a1821aadb04ffb24c1398936c434f52fe052
Kamil Karczmarczyk <k.karczmarczyk11@gmail.com>
2023-05-15 23:37:19 +0200

    Add DB_creation - SQL script for creation database, tables, insert values from .csv files, and establishing relationships among tables

---
+++ DB_creation.sql
@@ -0,0 +1,208 @@
+USE Olist_DB;
+GO
+-- customers
+DROP TABLE IF EXISTS customers;
+GO
+CREATE TABLE customers (
+	 customer_id varchar(100)
+	,customer_unique_id varchar(100)
+	,customer_zip_code_prefix varchar(20)
+	,customer_city varchar(100)
+	,customer_state varchar(20)
+	,CONSTRAINT PK_customers PRIMARY KEY ( customer_id )
+);
+GO
+BULK INSERT customers
+FROM 'D:\Data Science\projekty\archive (4)\olist_customers_dataset.csv'
+WITH ( 
+	FIRSTROW = 2,
+	ROWTERMINATOR = '0x0a',
+	FIELDTERMINATOR = ',',
+	FORMAT = 'CSV'
+	);
+GO
+-- geolocation
+DROP TABLE IF EXISTS geolocation;
+GO
+CREATE TABLE geolocation (
+	 geolocation_zip_code_prefix varchar(20)
+	,geolocation_lat float(50)
+	,geolocation_lng float(50)
+	,geolocation_city varchar(100)
+	,geolocation_state varchar(20)
+	-- TUTAJ DODAJ PRIMARY KEY o ile istnieje --
+);
+GO
+BULK INSERT geolocation
+FROM 'D:\Data Science\projekty\archive (4)\olist_geolocation_dataset.csv'
+WITH ( 
+	FIRSTROW = 2,
+	ROWTERMINATOR = '0x0a',
+	FIELDTERMINATOR = ',',
+	FORMAT = 'CSV'
+	);
+GO
+-- order_items
+DROP TABLE IF EXISTS order_items;
+GO
+CREATE TABLE order_items (
+	 order_id varchar(100)
+	,order_item_id int
+	,product_id varchar(100)
+	,seller_id varchar(100)
+	,shipping_limit_date datetime2
+	,price money
+	,freight_value money
+	,CONSTRAINT PK_order_items PRIMARY KEY (order_id,order_item_id)
+	,CONSTRAINT FK_orders FOREIGN KEY (order_id) REFERENCES orders(order_id)
+	,CONSTRAINT FK_order_items FOREIGN KEY (product_id) REFERENCES products(product_id)
+	,CONSTRAINT FK_order_sellers FOREIGN KEY (seller_id) REFERENCES sellers(seller_id)	
+	);
+GO
+BULK INSERT order_items
+FROM 'D:\Data Science\projekty\archive (4)\olist_order_items_dataset.csv'
+WITH ( 
+	FIRSTROW = 2,
+	ROWTERMINATOR = '0x0a',
+	FIELDTERMINATOR = ',',
+	FORMAT = 'CSV'
+	);
+GO
+-- order_payments
+DROP TABLE IF EXISTS order_payments;
+GO
+CREATE TABLE order_payments (
+	 order_id varchar(100)
+	,payment_sequential int
+	,payment_type varchar(100)
+	,payment_installments int
+	,payment_value money
+	,CONSTRAINT PK_order_payments PRIMARY KEY (order_id, payment_sequential) 
+	,CONSTRAINT FK_payments FOREIGN KEY (order_id) REFERENCES orders(order_id)
+	);
+GO
+BULK INSERT order_payments
+FROM 'D:\Data Science\projekty\archive (4)\olist_order_payments_dataset.csv'
+WITH ( 
+	FIRSTROW = 2,
+	ROWTERMINATOR = '0x0a',
+	FIELDTERMINATOR = ',',
+	FORMAT = 'CSV'
+	);
+GO
+-- order_reviews
+DROP TABLE IF EXISTS order_reviews;
+GO
+CREATE TABLE order_reviews (
+	 review_id varchar(100)
+	,order_id varchar(100)
+	,review_score int
+	,review_comment_title varchar(2500)
+	,review_comment_message varchar(2500)
+	,review_creation_date datetime2
+	,review_answer_timestamp datetime2
+	,CONSTRAINT PK_order_reviews PRIMARY KEY(review_id, order_id) 
+	,CONSTRAINT FK_orders_reviews FOREIGN KEY(order_id) REFERENCES orders(order_id)
+	);
+GO
+BULK INSERT order_reviews
+FROM 'D:\Data Science\projekty\archive (4)\olist_order_reviews_dataset.csv'
+WITH ( 
+	FIRSTROW = 2,
+	ROWTERMINATOR = '\n',
+	FIELDTERMINATOR = ',',
+	FORMAT = 'CSV',
+	FIELDQUOTE = '"'
+	);
+GO
+-- orders
+DROP TABLE IF EXISTS orders;
+GO
+CREATE TABLE orders (
+	 order_id varchar(100)
+	,customer_id varchar(100)
+	,order_status varchar(100)
+	,order_purchase_timestamp datetime2
+	,order_approved_at datetime2
+	,order_delivered_carrier_date datetime2
+	,order_delivered_customer_date datetime2
+	,order_estimated_delivery_date datetime2
+	,CONSTRAINT PK_orders PRIMARY KEY(order_id) 
+	,CONSTRAINT FK_customers FOREIGN KEY(customer_id) REFERENCES customers(customer_id)
+	);
+GO
+BULK INSERT orders
+FROM 'D:\Data Science\projekty\archive (4)\olist_orders_dataset.csv'
+WITH ( 
+	FIRSTROW = 2,
+	ROWTERMINATOR = '0x0a',
+	FIELDTERMINATOR = ',',
+	FORMAT = 'CSV'
+	);
+GO
+-- products
+DROP TABLE IF EXISTS products;
+GO
+CREATE TABLE products (
+	 product_id varchar(100)
+	,product_category_name varchar(100)
+	,product_name_lenght int
+	,product_description_lenght int
+	,product_photos_qty int
+	,product_weight_g float(20)
+	,product_length_cm float(20)
+	,product_height_cm float(20)
+	,product_width_cm float(20)
+	,CONSTRAINT PK_products PRIMARY KEY(product_id) 
+	,CONSTRAINT FK_category_name_translation FOREIGN KEY(product_category_name) REFERENCES product_category_name_translation(product_category_name)
+	);
+GO
+BULK INSERT products
+FROM 'D:\Data Science\projekty\archive (4)\olist_products_dataset.csv'
+WITH ( 
+	FIRSTROW = 2,
+	ROWTERMINATOR = '0x0a',
+	FIELDTERMINATOR = ',',
+	FORMAT = 'CSV'
+	);
+GO
+-- sellers
+DROP TABLE IF EXISTS sellers;
+GO
+CREATE TABLE sellers (
+	 seller_id varchar(100)
+	,seller_zip_code_prefix varchar(20)
+	,seller_city varchar(100)
+	,seller_state varchar(20)
+	,CONSTRAINT PK_sellers PRIMARY KEY(seller_id)
+	);
+GO
+BULK INSERT sellers
+FROM 'D:\Data Science\projekty\archive (4)\olist_sellers_dataset.csv'
+WITH ( 
+	FIRSTROW = 2,
+	ROWTERMINATOR = '0x0a',
+	FIELDTERMINATOR = ',',
+	FORMAT = 'CSV'
+	);
+GO
+-- product_category_name_translation
+DROP TABLE IF EXISTS product_category_name_translation;
+GO
+CREATE TABLE product_category_name_translation (
+	 product_category_name varchar(100)
+	,product_category_name_english varchar(100)
+	,CONSTRAINT PK_product_category_name_translation PRIMARY KEY(product_category_name)
+	);
+GO
+BULK INSERT product_category_name_translation
+FROM 'D:\Data Science\projekty\archive (4)\product_category_name_translation.csv'
+WITH ( 
+	FIRSTROW = 2,
+	ROWTERMINATOR = '0x0a',
+	FIELDTERMINATOR = ',',
+	FORMAT = 'CSV'
+	);
+GO
+
+-- jak przylaczyc geolocation?
