98b49ffbb214a4b01227f7bdb0868619502db3e8
Kamil Karczmarczyk <k.karczmarczyk11@gmail.com>
2023-05-17 00:24:48 +0200

    Add grouped_orders_CTE. write test.1 to investigate duplicates order_id problem

--- question_analysys.sql
+++ question_analysys.sql
@@ -1,5 +1,7 @@
--- QUESTIONS FROM STAKEHOLDERS
 USE Olist_DB;
+
+-- QUESTIONS FROM STAKEHOLDERS
+
 --	1) HOW MANY ORDERS IN DATABASE? 
 SELECT 
 	COUNT(*) 
@@ -12,16 +14,16 @@ SELECT
 FROM
 	customers
 
--- 3) LOYAL CLIENTS  - list of customers who had done more than 1 purchase
+-- 3) LOYAL CLIENTS  - list of customers who made more than one purchase
 SELECT
 	 customer_unique_id
 	,COUNT(*) AS purchase_count
 FROM customers
 GROUP BY customer_unique_id
 HAVING COUNT(*) > 1
-ORDER BY purchase_count DESC
+ORDER BY purchase_count DESC 
 
--- HOW MANY LOYAL CLIENTS(more than one purchase)
+-- 4) HOW MANY LOYAL CLIENTS(more than one purchase)
 
 SELECT COUNT(*)
 FROM (
@@ -32,13 +34,15 @@ FROM (
 	GROUP BY customer_unique_id
 	HAVING COUNT(*) > 1) AS loyal_client
 
---	How often is the average order value for a second purchase, larger than the first purchase?
-WITH orders_CTE (order_id, customer_unique_id, payment_value)
+-- 5) How often is the average order value for a second purchase, larger than the first purchase?
+
+WITH orders_CTE (order_id, customer_unique_id, payment_value,order_purchase_timestamp)
 AS (
-	SELECT top 1000 
+	SELECT
 		 OP.order_id
 		,C.customer_unique_id
 		,OP.payment_value
+		,O.order_purchase_timestamp
 	FROM order_payments AS OP
 	INNER JOIN 
 		orders AS O
@@ -54,20 +58,79 @@ AS (
 			GROUP BY customer_unique_id
 			HAVING COUNT(*) > 1
 		)
-	ORDER BY C.customer_unique_id
+),
+grouped_orders_CTE (order_id, customer_unique_id, order_purchase_timestamp, sum_payment_value)
+AS (
+	SELECT 
+		order_id
+		,customer_unique_id
+		,order_purchase_timestamp
+		,SUM(payment_value)
+	FROM orders_CTE
+	GROUP BY 
+		order_id
+		,customer_unique_id
+		,order_purchase_timestamp
 )
-SELECT top 1000 *
-FROM orders_CTE; --zwr�c w tym zapytaniu sumy payment_value per orderid w funkcji klienta unique_id
+-- this is query for selection earliest purchase for each customer
+SELECT 
+	 order_id
+	,customer_unique_id
+	,sum_payment_value
+	,FIRST_VALUE(sum_payment_value) OVER (PARTITION BY customer_unique_id ORDER BY order_purchase_timestamp ASC) AS customer_first_purchase_amount
+	,order_purchase_timestamp
+FROM grouped_orders_CTE
+WHERE customer_unique_id ='9a736b248f67d166d2fbb006bcb877c3'
+ORDER BY order_purchase_timestamp
 
--- ponizej lista id klient�w kt�rzy zrobili wiecej ni� 1 zam�wienie
-SELECT
-	customer_unique_id
-FROM customers
-GROUP BY customer_unique_id
-HAVING COUNT(*) > 1
+
+/* test 1 (go investigate ''top'' client, why 31 payments? maybe this is number of installments)
+should i group in grouped_orders_CTE or should i consider this data in other way? check for dbo.order_payments*/
+
+/*
+WITH orders_CTE (order_id, customer_unique_id, payment_value,order_purchase_timestamp)
+AS (
+	SELECT
+		 OP.order_id
+		,C.customer_unique_id
+		,OP.payment_value
+		,O.order_purchase_timestamp
+	FROM order_payments AS OP
+	INNER JOIN 
+		orders AS O
+		ON OP.order_id = O.order_id
+	INNER JOIN 
+		customers AS C
+		ON c.customer_id = O.customer_id
+	WHERE
+		C.customer_unique_id IN (
+			SELECT
+				customer_unique_id
+			FROM customers
+			GROUP BY customer_unique_id
+			HAVING COUNT(*) > 1
+		)
+)
+
+
+SELECT 
+	 order_id
+	,customer_unique_id
+	,payment_value
+	,FIRST_VALUE(payment_value) OVER (PARTITION BY customer_unique_id ORDER BY order_purchase_timestamp DESC) AS top_purchase_of_customer
+	,order_purchase_timestamp
+FROM orders_CTE
+WHERE customer_unique_id ='9a736b248f67d166d2fbb006bcb877c3'
+ORDER BY order_purchase_timestamp
+*/
+
+
+
+--9a736b248f67d166d2fbb006bcb877c3 TOP KLIENT
+
 --	What is the average order value by city?
 
---	What�s the total value of orders that haven�t been delivered?
+--	What's the total value of orders that haven't been delivered?
 
 
 
